fbfabba7e403de06052f93e78cb0d923e0c41859
Harsh Jaykumar Jalan <harshjalan27@yahoo.com>
2026-03-10 23:37:31 -0700

    Add solution for dropout-training

---
+++ dropout-training/dropout-training.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def dropout(x, p=0.5, rng=None):
+    """
+    Apply dropout to input x with probability p.
+    Return (output, dropout_pattern).
+    """
+    # Write code here
+    x = np.array(x)
+    if rng:
+        random_arr = rng.random((x.shape))
+    else:
+        random_arr = np.random.random((x.shape))
+    print(random_arr)
+    dropout_pattern = np.where(random_arr<(1-p), 1/(1-p), 0)
+    return x*dropout_pattern, dropout_pattern
