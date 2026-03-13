59a928a98a20be5b96cf88e4b185936ae0e87cfe
Harsh Jaykumar Jalan <harshjalan27@yahoo.com>
2026-03-12 23:19:27 -0700

    Update solution for k-means-centroid-update

--- k-means-centroid-update/k-means-centroid-update.py
+++ k-means-centroid-update/k-means-centroid-update.py
@@ -11,6 +11,5 @@ def k_means_centroid_update(points, assignments, k):
         cluster_idx = assignments[i]
         unique_counts[cluster_idx]+=1
         centroids[cluster_idx] += points[i]
-    print(centroids)
     centroids =  [(centroids[i]/unique_counts[i]).tolist() if unique_counts[i]>0 else centroids[i].tolist() for i in range(k)]
     return centroids
