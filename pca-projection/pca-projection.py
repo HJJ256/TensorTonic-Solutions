2de63713af62d3159751b7782a8b78b0ba732cdf
Harsh Jaykumar Jalan <harshjalan27@yahoo.com>
2026-03-12 22:43:55 -0700

    Update solution for pca-projection

--- pca-projection/pca-projection.py
+++ pca-projection/pca-projection.py
@@ -6,7 +6,7 @@ def pca_projection(X, k):
     """
     X = np.array(X) #NxD
     X_bar = np.mean(X, axis=0) # (D,) mean for each feature
-    X_c = X-X_bar #N,D
+    X_c = X-X_bar #N,D X_bar gets auto broadcasted
     N,_= X_c.shape
     C = (X_c.T.dot(X_c))/(N-1)
     eig_val, eig_vec = np.linalg.eig(C)
